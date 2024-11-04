a91563ce250798d80ebbc5f8c2f7e0322bc312ce
REN-22 <renzo.bastide@gmail.com>
2024-11-04 10:22:12 +0100

    hash

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import connexion from './db_connexion';
 import express from 'express';
 import mysql from 'mysql2/promise';
+import { hashPassword,comparePassword } from './hashage';
 
 const app = express();
 const port = 5000;
@@ -9,16 +10,17 @@ const port = 5000;
 app.use(express.json());
 
 
+/*------------------------------------------POST---------------------------------------------- */
 
-
-app.post('/create-user', async (req, res) => {
+app.post('/POST/create-user', async (req, res) => {
     const { pseudo, nom, prenom, adresse, cp, ville, telephone, mail, mdp, role, statut, notif_mail, statut_cotisation } = req.body;
+    const hashedMdp = hashPassword(mdp);
     try {
         const result = await connexion.execute(
             `INSERT INTO utilisateur (pseudo, nom, prenom, adresse, cp, ville, telephone, mail, mdp, role, statut, notif_mail, statut_cotisation) 
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
 
-            [pseudo, nom, prenom, adresse, cp, ville, telephone, mail, mdp, role, statut, notif_mail, statut_cotisation]
+            [pseudo, nom, prenom, adresse, cp, ville, telephone, mail, hashedMdp, role, statut, notif_mail, statut_cotisation]
         );
 
         res.status(201).json({
@@ -45,3 +47,15 @@ app.post('/create-user', async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+
+/*------------------------------------------GET---------------------------------------------- */
+
+
+
+
+
+
+
+
+/*-----------------------------------------DELETE---------------------------------------------- */
+
